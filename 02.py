0741f6d9b7bb18d6c2752539ef9bfe78fa1552f4
unknown <krajat@mirafra.com>
2022-09-30 11:56:43 +0530

    Simple Code

---
+++ 02.py
@@ -0,0 +1,10 @@
+#Here we Look into Some Basic Strings methods
+
+Book = "The legend of MahaBharata"
+
+print(len(Book))
+print(Book.capitalize())
+print(Book.upper())
+print(Book.lower())
+print(Book.find("legend"))
+print("Mahabharata"in Book)
