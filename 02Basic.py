36a5c4920050953cd626438c51925e6101ba79f4
unknown <krajat@mirafra.com>
2022-09-30 12:39:24 +0530

    Concetenate_String

--- 02Basic.py
+++ 02Basic.py
@@ -1,10 +1,15 @@
-
-
-Name1 = "Rajat kumar"
-Name2 = "Manmohan"
-Name3 = "Shorya"
+name1 = "Rajat kumar"
+name2 = "Manmohan"
+name3 = "Shorya"
 
 
 #COncetenate the String 
 
-print(Name1 + " " + Name2 + " " + Name3)
+print(name1 + " " + name2 + " " + name3)
+
+
+#Input from user
+
+name = input("what is yours name? ")
+color = input("what is yours fav color? ")
+print(name + ' likes ' + color)
